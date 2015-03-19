6f54f1280ae41b2e34f179d3b29c03540f444572
Abhishek <abhishek.shanthkumar@gmail.com>
2015-03-19 14:14:28 +0530

    Version 1.2.1: Removed image buttons from All Deals and Nearby Deals.

--- src/com/project/waverr/DealPage.java
+++ src/com/project/waverr/DealPage.java
@@ -34,14 +34,15 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.project.waverr.SimpleGestureFilter.SimpleGestureListener;
 import com.squareup.picasso.Picasso;
+//import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class DealPage extends GlobalActionBar implements OnTabChangeListener, /*OnMapReadyCallback,*/ OnClickListener, SimpleGestureListener{
+public class DealPage extends GlobalActionBar implements OnTabChangeListener, OnMapReadyCallback, OnClickListener, SimpleGestureListener{
 
 	TabHost th;
 	TextView x, restname, dealtext, duration, instructions, finePrint;
@@ -253,7 +254,7 @@ public class DealPage extends GlobalActionBar implements OnTabChangeListener, /*
 
 	}
 
-	/*@Override
+	@Override
 	public void onMapReady(GoogleMap map) {
 		// TODO Auto-generated method stub
 
@@ -261,7 +262,7 @@ public class DealPage extends GlobalActionBar implements OnTabChangeListener, /*
 		map.addMarker(new MarkerOptions()
 		.position(new LatLng(latitude, longitude))
 		.title("Location"));
-	}*/
+	}
 
 	@Override
 	public void onClick(View v) {
@@ -640,7 +641,7 @@ public class DealPage extends GlobalActionBar implements OnTabChangeListener, /*
 		longitude = Double.parseDouble(latlng[1]);
 
 		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.the_map);
-	//	mapFragment.getMapAsync(this);
+		mapFragment.getMapAsync(this);
 	}
 
 	private void closeActivity() {
